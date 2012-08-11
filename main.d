2414e099dac791397a82ac64a7a96ebab544fbaf
Jerry Fu <norcrel@gmail.com>
2012-08-11 10:03:15 -0700

    Added files from sensors example

---
+++ main.d
@@ -0,0 +1,115 @@
+main.o: main.cpp \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/syscall.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/types.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/audio.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/events.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/vram.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/asset.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/elf.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/array.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/memory.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/macros.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/group.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/limits.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/math.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/loader.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/audio.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/image.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/audio.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/cube.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/event.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/filesystem.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/metadata.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/motion.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/string.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/system.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/time.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/color.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/sprite.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/framebuffer.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg0rom.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg0.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg1.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg2.h \
+  /Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/syscall.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/types.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/audio.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/events.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/vram.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/asset.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/abi/elf.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/array.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/memory.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/macros.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/group.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/limits.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/math.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/loader.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/audio.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/asset/image.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/audio.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/cube.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/event.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/filesystem.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/metadata.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/motion.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/string.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/system.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/time.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/color.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/sprite.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/framebuffer.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg0rom.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg0.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg1.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/bg2.h:
+
+/Users/jfu/Downloads/sifteo-sdk-mac64-v0.9.6-30-gba4451b/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
